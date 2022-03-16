12da9f5a333c9cbf0a573f03d93a022ed5f0a5fd
ADMIN <ADMIN@DESKTOP-B9DFJR1>
2022-03-16 20:00:37 +0700

    Add project files.

---
+++ Tuan4_PhamKimThanh/Controllers/GioHangController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tuan4_PhamKimThanh.Models;
+
+namespace Tuan4_PhamKimThanh.Controllers
+{
+    public class GioHangController : Controller
+    {
+        // GET: GioHang
+        MyDataDataContext data = new MyDataDataContext();
+        public List<GioHang> Laygiohang()
+        {
+            List<GioHang> listGioHang = Session["GioHang"] as List<GioHang>;
+            if (listGioHang == null)
+            {
+                listGioHang = new List<GioHang>();
+                Session["GioHang"] = listGioHang;
+            }
+            return listGioHang;
+        }
+
+        public ActionResult ThemGioHang(int id, string strURl)
+        {
+            List<GioHang> listGioHang = Laygiohang();
+
+            GioHang sanpham = listGioHang.Find(n => n.masach == id);
+
+            if (sanpham == null)
+            {
+                sanpham = new GioHang(id);
+                listGioHang.Add(sanpham);
+                return Redirect(strURl);
+            }
+            else
+            {
+                sanpham.iSoluong++;
+                return Redirect(strURl);
+            }
+
+        }
+
+        private int TongSoLuong()
+        {
+            int tsl = 0;
+            List<GioHang> listGioHang = Session["GioHang"] as List<GioHang>;
+
+            if (listGioHang != null)
+            {
+                tsl = listGioHang.Sum(n => n.iSoluong);
+            }
+            return tsl;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Tuan4_PhamKimThanh/Models/GioHang.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Tuan4_PhamKimThanh.Models
+{
+    public class GioHang
+    {
+        MyDataDataContext data = new MyDataDataContext();
+        public int masach { get; set; }
+
+        [Display(Name = "Tên sách")]
+        public string tensach { get; set; }
+
+        [Display(Name = "Ảnh bìa")]
+        public string hinh { get; set; }
+
+        [Display(Name = "Giá bán")]
+        public Double giaban { get; set; }
+
+        [Display(Name = "Số lượng")]
+        public int iSoluong { get; set; }
+
+        [Display(Name = "Thành tiền")]
+        public Double dThanhtien
+        {
+            get
+            {
+                return iSoluong * giaban;
+            }
+        }
+    }
+}
